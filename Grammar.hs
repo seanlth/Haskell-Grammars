79e6f3dd601924a098614f703b5112ba37c67b86
seanlth <seanlthcamera@gmailc.om>
2015-04-18 20:33:05 +0100

    fixed bugs

--- Grammar.hs
+++ Grammar.hs
@@ -26,12 +26,14 @@ getNodeValue (NonTerm x) = (2, x)
 getNodeValue (Prod x ns) = (2, x)
 
 -- The Language --
-prods = [stmts, stmt, stmt1, var, var1, num, cond, cond1]
+prods = [top, stmt, stmt1, var, var1, var2, num, cond, cond1]
+top = Prod "s" [NonTerm "stmt"]
 stmt = Prod "stmt" [Term "if", NonTerm "cond", Term "then", NonTerm "stmt", Term "else", NonTerm "stmt"]
 stmt1 = Prod "stmt" [NonTerm "var", Term "=", NonTerm "num"]
 stmts = Prod "stmts" [NonTerm "stmt", Term ";", NonTerm "stmt"]
 var = Prod "var" [Term "x"]
 var1 = Prod "var" [Term "y"]
+var2 = Prod "var" [Eps]
 num = Prod "num" [Term "1"]
 cond = Prod "cond" [Term "TRUE"]
 cond1 = Prod "cond" [Term "FALSE"]
@@ -45,12 +47,16 @@ resolve (p:ps) v@(NonTerm x) | x == getProdName p = [p] ++ resolve ps v
                              | otherwise = resolve ps v
 resolve _ _ = []
 
+-- Are symbols nullable
+nullable' :: [Node] -> [Node] -> Bool
+nullable' t s = foldr (&&) True (map (nullable t) s) 
+
 -- Is symbol nullable --
 nullable :: [Node] -> Node -> Bool
 nullable _ Eps = True
 nullable _ (Term _) = False
 nullable t n@(NonTerm x) = let ps = (resolve t n) in foldr (||) False (map (nullable t) ps)
-nullable t p@(Prod x (n:ns)) = nullable t n
+nullable t p@(Prod x (ns)) = foldr (&&) True (map (nullable t) ns)
 
 -- First set of list of symbols --
 first' :: [Node] -> [Node] -> [String]
@@ -63,8 +69,7 @@ first :: [Node] -> Node -> [String]
 first _ Eps = ["eps"]
 first _ (Term x) = [x]
 first t n@(NonTerm x) = let ps = (resolve t n) in foldr (union) [] (map (first t) ps)
-first t p@(Prod x c@(n:ns)) | nullable t p = (first' t c) 
-                            | otherwise = first t n
+first t p@(Prod x c@(n:ns)) = (first' t c) 
 
 
 -- Follow set for production --
@@ -82,8 +87,8 @@ follow _ _ = []
 
 
 select :: [Node] -> Node -> [String]
-select t p@(Prod m ns) | nullable t p = union (first t p) (follow t p)
-                       | otherwise = first t p
+select t p@(Prod m c@(n:ns)) | nullable' t c = union (first' t c \\ ["eps"]) (follow t p)
+                             | otherwise = first' t c
 select _ _ = error "not a production"
 
 
